876eede75178941638ba879e536682c19537d9ee
Sonu Ahirwar <54439308+Pravesh-sahu@users.noreply.github.com>
2026-01-09 14:37:25 +0530

    Update Footer.jsx

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -5,16 +5,16 @@ function Footer() {
     <footer className="p-4 mt-4">
       {/* <div><a href="mailto:sonuahirwarak210@gmail.com">sonuahirwarak210@gmail.com</a></div> */}
       <div className="social-div    d-flex justify-content-center ">
-        <a href="https://www.linkedin.com/in/sonu-ahirwar-4628331a0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="d-flex align-items-center bg-color-dark p-2 rounded">
+        <a href="https://www.linkedin.com/in/sonu-ahirwar-4628331a0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="d-flex align-items-center bg-color-dark p-2 rounded">
           <FaLinkedin />
         </a>
-        <a href="https://www.instagram.com/sonuahirwar_/?igsh=dWp6bTJha2lqeDhy" className="d-flex align-items-center bg-color-dark p-2 rounded">
+        <a href="https://www.instagram.com/sonuahirwar_/?igsh=dWp6bTJha2lqeDhy" target="_blank" className="d-flex align-items-center bg-color-dark p-2 rounded">
           <FaInstagram />
         </a>
         <a href="https://www.facebook.com/sonu.ahirwar.5070" className="d-flex align-items-center bg-color-dark p-2 rounded">
           <FaFacebook />
         </a>
-        <a href="https://wa.me/7354859868" className="d-flex align-items-center bg-color-dark p-2 rounded">
+        <a href="https://wa.me/7354859868" target="_blank" className="d-flex align-items-center bg-color-dark p-2 rounded">
           <FaWhatsapp />
         </a>
             <a href="mailto:sonuahirwarak212@gmail.com"  className="d-flex align-items-center bg-color-dark p-2 rounded">
