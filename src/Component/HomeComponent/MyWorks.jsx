7537070d70a57e0e8dd2e7b48e80f1329993cb93
Sonu Ahirwar <54439308+Pravesh-sahu@users.noreply.github.com>
2026-01-09 15:46:02 +0530

    Update MyWorks.jsx

--- src/Component/HomeComponent/MyWorks.jsx
+++ src/Component/HomeComponent/MyWorks.jsx
@@ -55,51 +55,51 @@ function About() {
         <div class="row slider-right position-relative z-1">
         <Slider {...settings}>
           <div className="slide">
-                <a href="https://nilohealth.com/" target="_blank" className="d-block position-relative h-100 overflow-hidden">
+                <a href="https://nilohealth.com/"  className="d-block position-relative h-100 overflow-hidden">
               <img src={project1} className="w-100" alt="my-profile" />
               <div className="content"><h5>NILOHEALTH</h5><button className="dark-btn mt-2">Visit Website</button></div>
             </a>
           </div>
 
           <div className="slide">
-                <a href="https://raan.com/" target="_blank" className="d-block position-relative h-100 overflow-hidden">
+                <a href="https://raan.com/"  className="d-block position-relative h-100 overflow-hidden">
               <img src={project2} className="w-100" alt="my-profile" />
               <div className="content"><h5>RAAN</h5><button className="dark-btn mt-2">Visit Website</button></div>
             </a>
           </div>
           <div className="slide">
-                <a href="https://podcastmonkey.co/" target="_blank" className="d-block position-relative h-100 overflow-hidden">
+                <a href="https://podcastmonkey.co/"  className="d-block position-relative h-100 overflow-hidden">
               <img src={project7} className="w-100" alt="my-profile" />
               <div className="content"><h5>POADCASTMONKEY</h5><button className="dark-btn mt-2">Visit Website</button></div>
             </a>
           </div>
           <div className="slide">
-                <a href="https://hastee.com/" target="_blank" className="d-block position-relative h-100 overflow-hidden">
+                <a href="https://hastee.com/"  className="d-block position-relative h-100 overflow-hidden">
               <img src={project3} className="w-100" alt="my-profile" />
               <div className="content"><h5>HASTEE</h5><button className="dark-btn mt-2">Visit Website</button></div>
             </a>
           </div>
           <div className="slide">
-                <a href="https://www.jusre.be/" target="_blank" className="d-block position-relative h-100 overflow-hidden">
+                <a href="https://www.jusre.be/"  className="d-block position-relative h-100 overflow-hidden">
               <img src={project4} className="w-100" alt="my-profile" />
               <div className="content"><h5>JUSRE</h5><button className="dark-btn mt-2">Visit Website</button></div>
             </a>
           </div>
           <div className="slide">
-                <a href="https://thenaturalcraft.com/" target="_blank" className="d-block position-relative h-100 overflow-hidden">
+                <a href="https://thenaturalcraft.com/"  className="d-block position-relative h-100 overflow-hidden">
               <img src={project5} className="w-100" alt="my-profile" />
               <div className="content"><h5>NATURAL CRAFT</h5><button className="dark-btn mt-2">Visit Website</button></div>
             </a>
           </div>
           <div className="slide">
-                <a href="https://visitas.haciendaguzman.com/" target="_blank" className="d-block position-relative h-100 overflow-hidden">
+                <a href="https://visitas.haciendaguzman.com/"  className="d-block position-relative h-100 overflow-hidden">
               <img src={project6} className="w-100" alt="my-profile" />
               <div className="content"><h5>Haciendaguzman</h5><button className="dark-btn mt-2">Visit Website</button></div>
             </a>
           </div>
       
           <div className="slide">
-                <a href="https://cosmoclinic.it/" target="_blank" className="d-block position-relative h-100 overflow-hidden">
+                <a href="https://cosmoclinic.it/"  className="d-block position-relative h-100 overflow-hidden">
               <img src={project8} className="w-100" alt="my-profile" />
               <div className="content"><h5>COSMO CLINIC</h5><button className="dark-btn mt-2">Visit Website</button></div>
             </a>
